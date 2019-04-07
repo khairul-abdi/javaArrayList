bace85c1e322d45f8b8a5c239023aa36a37f1ee9
khairul-abdi <kabdi384@gmail.com>
2019-04-07 17:52:01 +0700

    first commit

---
+++ src/arrayList.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class arrayList {
+    public static void main(String[] args) {
+
+            //membuat objek array list
+        ArrayList kantongAjaib = new ArrayList();
+
+        //Mengisi kantong ajaib dengan 5 benda
+        kantongAjaib.add("Senter Pembesar");
+        kantongAjaib.add(532);
+        kantongAjaib.add("tikus");
+        kantongAjaib.add(1231234.132);
+        kantongAjaib.add(true);
+
+        //menghapus tikus dari kantong ajaib
+        kantongAjaib.remove("tikus");
+
+        //menampilkan isi kantong ajaib
+        System.out.println(kantongAjaib);
+
+        //Menampilkan banyak isi kantong ajaib
+        System.out.println("Kantong ajaib berisi "+kantongAjaib.size()+"item");
+
+    }
+}
